30d5a2c197491307fb3962612fad64119bb320b0
Nathan Duff <me@nateduff.com>
2024-02-16 15:08:07 -0500

    Testing ping

--- n8cams/run.sh
+++ n8cams/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
 MESSAGE=$(bashio::config 'message')
+HOSTNAME=$(bashio::config 'hostName')
 
 echo "option message \"${MESSAGE}\";"
+echo "option hostName \"${HOSTNAME}\";"
+
+ping -c1 $HOSTNAME

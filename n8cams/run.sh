e1ee70f9f79684ed4d7662ac5575d9f7aa43f8bb
Nathan Duff <me@nateduff.com>
2024-02-19 12:54:14 -0500

    Stop the ping

--- n8cams/run.sh
+++ n8cams/run.sh
@@ -6,6 +6,6 @@ HOSTNAME=$(bashio::config 'hostName')
 echo "option message \"${MESSAGE}\";"
 echo "option hostName \"${HOSTNAME}\";"
 
-ping -c1 $HOSTNAME
+#ping -c1 $HOSTNAME
 
 python3 -m http.server 8000

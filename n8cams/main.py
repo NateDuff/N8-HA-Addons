0cdc8d8d86117a4091c42786e1ffc1465a7ce688
Nathan Duff <me@nateduff.com>
2024-02-19 13:31:15 -0500

    No extra installs

--- n8cams/main.py
+++ n8cams/main.py
@@ -1,16 +1,28 @@
-import http.server
-import socketserver
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
 PORT = 8000
 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+class WebRequestHandler(BaseHTTPRequestHandler):
+    def get_response(self):
+        return json.dumps(
+            {
+                "path": self.url.path,
+                "query_data": self.query_data,
+                "post_data": self.post_data.decode("utf-8"),
+                "form_data": self.form_data,
+            }
+        )
+    
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
-        self.wfile.write(b"Hey there world from python!!") # Response body
+        self.wfile.write(self.get_response().encode("utf-8"))
 
-# Create an HTTP server with custom request handler
-with socketserver.TCPServer(("", PORT), MyHttpRequestHandler) as httpd:
-    print("Server running at port", PORT)
-    httpd.serve_forever()
+    def do_POST(self):
+        self.do_GET()
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8000), WebRequestHandler)
+    server.serve_forever()
